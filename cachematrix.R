90d6fae952cebf46b236318205902634c12348c9
tobaccopipeyoyo <tobaccopipeyoyo@gmail.com>
2015-07-26 11:23:42 -0700

    First commit to Prog Assgn 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  invrMatrix <- NULL
+  get        <- function() x
+  set        <- function(y){
+    x          <<- y
+    invrMatrix <<- NULL
+  }#end of set()
+  
+  setInvr    <- function() {
+    invrMatrix <<- solve(x)
+  }#end of setInvr
+  
+  getInvr    <- function() invrMatrix
+  
+  list(get = get, set = set,
+       setInvr = setInvr,
+       getInvr = getInvr)
 }
 
 
@@ -12,4 +27,12 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        cacheInvr <- x$getInvr()
+        if(!is.null(cacheInvr)){
+                    message("Getting cached data")
+                    return(cacheInvr)
+        }#end of if
+        x$setInvr()
+        invrMatrix  <- x$getInvr()
+        invrMatrix
 }
